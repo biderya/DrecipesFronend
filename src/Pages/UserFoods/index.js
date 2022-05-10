55a369a962428806f3fc751e3fe86bebf2032a9b
Biderya <ganbold.biderya234@gmail.com>
2022-05-11 01:12:08 +0800

    hool nemeh hesgiig ehlev

--- src/Pages/UserFoods/index.js
+++ src/Pages/UserFoods/index.js
@@ -3,16 +3,64 @@ import axios from "../../axios";
 import { Spinner } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { Card } from "react-bootstrap";
-import { Row, Col, InputGroup, FormControl } from "react-bootstrap";
+import {
+  Row,
+  Col,
+  InputGroup,
+  FormControl,
+  Table,
+  Button,
+  Modal,
+  Form,
+  Alert,
+} from "react-bootstrap";
 import MyLayout from "../../Components/MyLayout";
 import { SERVERAPI } from "../../constants/routes";
+import foodImg from "file:///C:/Users/user/OneDrive/Desktop/diplom_re/DrecipesFronend/src/assets/food.jpg";
+import "./style.css";
 
 const UserFoods = () => {
   const [id, setId] = useState(null);
   const [error, setError] = useState(null);
+  const [data, setData] = useState(null);
   const [loading, setLoading] = useState(false);
   const [foods, setFoods] = useState([]);
+  const [refresh, setRefresh] = useState(false);
+  const [showAlert, setShowAlert] = useState(false);
+  const [show, setShow] = useState(false);
+  const [showNew, setShowNew] = useState(false);
   const [searchValue, setSearchValue] = useState("");
+  const [newFoodName, setNewFoodName] = useState(null);
+  const [newFoodPhoto, setNewFoodPhoto] = useState(null);
+  const [newFoodContent, setNewFoodContent] = useState(null);
+  const [newFoodCalorie, setNewFoodCalorie] = useState(null);
+  const [newFoodSteps, setNewFoodSteps] = useState(null);
+  const [newFoodBestSeller, setNewFoodBestSeller] = useState(null);
+  const [newFoodIngredients, setNewFoodIngredients] = useState(null);
+
+  const handleClose = () => {
+    setShow(false);
+    setShowNew(false);
+    setData(null);
+  };
+  const handleFoodName = (e) => {
+    setNewFoodName(e.target.value);
+  };
+  const handleFoodPhoto = (e) => {
+    setNewFoodPhoto(e.target.value);
+  };
+  const handleFoodContent = (e) => {
+    setNewFoodContent(e.target.value);
+  };
+  const handleFoodCalorie = (e) => {
+    setNewFoodCalorie(e.target.value);
+  };
+  const handleFoodSteps = (e) => {
+    setNewFoodSteps(e.target.value);
+  };
+  const handleFoodBestSeller = (e) => {
+    setNewFoodBestSeller(e.target.value);
+  };
 
   const onSub = (e) => {
     setSearchValue(e.target.value);
@@ -44,7 +92,38 @@ const UserFoods = () => {
         setLoading(false);
         setError(err.response);
       });
-  }, []);
+  }, [refresh]);
+  const showModal = () => {
+    setShowNew(true);
+  };
+  const addFood = () => {
+    setLoading(true);
+    axios
+      .post(`foods`, {
+        name: newFoodName,
+        photo: newFoodPhoto,
+        content: newFoodContent,
+        calorie: newFoodCalorie,
+        author: setId(localStorage.getItem("id")),
+        rating: 4,
+        steps: newFoodSteps,
+        bestseller: newFoodBestSeller,
+        success: true,
+        deleted: null,
+        loading: false,
+      })
+      .then((result) => {
+        console.log(result.data.data);
+        setLoading(false);
+        setShowAlert(true);
+      })
+      .catch((err) => {
+        console.log(err.message);
+        setLoading(false);
+      });
+    setShowNew(false);
+    setRefresh(true);
+  };
 
   return (
     <MyLayout>
@@ -71,35 +150,136 @@ const UserFoods = () => {
               />
             </InputGroup>
           </div>
+          <div
+            className="d-flex justify-content-start"
+            class="w-100 p-1"
+            style={{ height: 0 }}
+          >
+            <Alert show={showAlert} variant="success">
+              {/* <Alert.Heading>Амжилттай өөрчлөгдлөө!</Alert.Heading> */}
+              <div className="d-flex justify-content-between">
+                <p>Таны хийсэн үйлдэл амжилттай боллоо!</p>
+                <Button
+                  style={{
+                    display: "block",
+                    marginTop: 3,
+                    width: 100,
+                  }}
+                  size="sm"
+                  onClick={() => setShowAlert(false)}
+                  variant="success"
+                >
+                  Хаах
+                </Button>
+              </div>
+            </Alert>
+          </div>
 
-          <Row md="12">
+          <Row md="12" className="container-fluid contenedor text-center">
             {filteredFoods.map((el) => (
-              <Col md="3" key={el._id}>
+              <Col md="4" key={el._id}>
                 <Link
                   to={`/foods/${el._id}`}
                   style={{ textDecoration: "none", color: "black" }}
                 >
-                  <Card className={`fj-card`} style={{ marginBottom: "20px" }}>
-                    <div className="card-body-wrapper">
-                      <div className="view overlay">
-                        <Card.Img
-                          style={{ width: 253, height: 220 }}
-                          variant="top"
-                          src={`${SERVERAPI}/upload/` + el.photo}
-                        />
+                  <div className="">
+                    <div className=" container text-center">
+                      <div className="container_foto">
+                        <div className="ver_mas text-center">
+                          <h2>Үзэх</h2>
+                        </div>
+                        <article className="text-left">
+                          <h2>{el.name}</h2>
+                          <h4>{el.content}</h4>
+                        </article>
+                        <img src={foodImg} alt />
                       </div>
-                      <Card.Body>
-                        <Card.Title className="card-main-title">
-                          {el.name}
-                        </Card.Title>
-                        <Card.Text>{el.content}</Card.Text>
-                      </Card.Body>
                     </div>
-                  </Card>
+                  </div>
                 </Link>
               </Col>
             ))}
           </Row>
+          <div>
+            <Button
+              style={{ marginBottom: 50 }}
+              variant="success"
+              type="submit"
+              onClick={showModal}
+            >
+              Шинэ орц нэмэх
+            </Button>
+            <Modal show={showNew} onHide={handleClose}>
+              <Modal.Header closeButton>
+                <Modal.Title>Шинэ орцын мэдээлэл</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <Form>
+                  <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>орцын нэр</Form.Label>
+                    <Form.Control
+                      type="text"
+                      placeholder="орцын нэр"
+                      onChange={(e) => {
+                        handleFoodName(e);
+                      }}
+                    />
+                    <Form.Text className="text-muted"></Form.Text>
+                  </Form.Group>
+                  <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>орцын calorie</Form.Label>
+                    <Form.Control
+                      type="text"
+                      placeholder="орцын calorie"
+                      onChange={(e) => {
+                        handleFoodCalorie(e);
+                      }}
+                    />
+                    <Form.Text className="text-muted"></Form.Text>
+                  </Form.Group>
+                  <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>True ymu False</Form.Label>
+                    <Form.Control
+                      type="text"
+                      placeholder="орцын нэр"
+                      onChange={(e) => {
+                        handleFoodBestSeller(e);
+                      }}
+                    />
+                    <Form.Text className="text-muted"></Form.Text>
+                  </Form.Group>
+                  <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>орцын тайлбар</Form.Label>
+                    <Form.Control
+                      as="textarea"
+                      rows={3}
+                      onChange={(e) => {
+                        handleFoodContent(e);
+                      }}
+                    />
+                  </Form.Group>
+                  <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>орцын алхам</Form.Label>
+                    <Form.Control
+                      as="textarea"
+                      rows={3}
+                      onChange={(e) => {
+                        handleFoodSteps(e);
+                      }}
+                    />
+                  </Form.Group>
+                </Form>
+              </Modal.Body>
+              <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                  Хаах
+                </Button>
+                <Button variant="success" type="submit" onClick={addFood}>
+                  Хадгалах
+                </Button>
+              </Modal.Footer>
+            </Modal>
+          </div>
         </div>
       )}
     </MyLayout>
